aaf1adc4415370a712b17254b9904d7911bf926a
Roman Rudi <roman.rudi@outlook.com>
2023-11-04 10:01:50 +0200

    Add randomSurprise5%, add callAll

--- handlers/eggs.py
+++ handlers/eggs.py
@@ -1,9 +1,10 @@
+import glob
 import re
 import random
 from utils.logger import STDOUT_LOGGER as logger
 from settings import STATIC_DIR
 from telegram import Update
-from telegram.ext import MessageHandler, ContextTypes, filters
+from telegram.ext import MessageHandler, ContextTypes, filters, CommandHandler
 
 EGGS_PATTERN = ".*([яЯ][иИЙй][ЧчЦц]).*"
 
@@ -12,7 +13,22 @@ async def eggs(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_message = update.message.text
     logger.info("[%s] %s", update.message.chat.username, user_message)
     if re.match(EGGS_PATTERN, user_message) is not None:
-        egg_number = random.randint(1, 4)
+        file_list = glob.glob(STATIC_DIR + '/*')
+        is_moshonka = random.randint(1, 100) <= 5
+        egg_number = 0 if is_moshonka else len(file_list)
+
         await context.bot.send_photo(update.effective_chat.id, photo=open(f'{STATIC_DIR}/eggs_{egg_number}.jpg', 'rb'))
 
+
+async def call_all(update: Update, context):
+    chat_id = update.message.chat_id
+    members = context.bot.get_chat_members(chat_id)
+
+    mention_all = " ".join([f"@{member.user.username}" for member in members if member.user.username])
+    await context.bot.send_message(chat_id, f"{mention_all}")
+
+
 EGGS_HANDLER = MessageHandler(filters.TEXT, eggs)
+CALL_ALL_HANDLER = CommandHandler('callAll', call_all)
+
+
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from telegram.ext import ApplicationBuilder
+
+from handlers.eggs import CALL_ALL_HANDLER
 from settings import TELEGRAM_TOKEN
 from handlers import *
 
@@ -6,6 +8,7 @@ app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
 app.add_handlers([
     EGGS_HANDLER,
+    CALL_ALL_HANDLER
 ])
 
 app.run_polling()
